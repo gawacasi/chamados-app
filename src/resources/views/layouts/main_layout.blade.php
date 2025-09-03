1fa1471cc12cd8bfab8938934c95d23ce39054ff
gawa <gwcs@cin.ufpe.br>
2025-09-03 17:42:51 -0300

    [ Layout ]

--- src/resources/views/layouts/main_layout.blade.php
+++ src/resources/views/layouts/main_layout.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NOTES</title>
+    <title>CHAMADOS</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href= "{{ asset('assets/fontawesome/css/all.min.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png')}}" type="image/png">
 </head>
 <body>
 
+    @yield('content')
 
     <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
 </body>

2ed5c3dc07c56a3e0f887ad8ee37070ced70f6c6
gawa <gwcs@cin.ufpe.br>
2025-09-04 05:02:40 -0300

    [ DumpSQL ]

--- dump-chamados.sql
+++ dump-chamados.sql
@@ -14,21 +14,55 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Copiando dados para a tabela laravel.chamados: ~2 rows (aproximadamente)
+-- Copiando estrutura para tabela laravel.chamados
+DROP TABLE IF EXISTS `chamados`;
+CREATE TABLE IF NOT EXISTS `chamados` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text` varchar(3000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('PEN','DEV','FIN') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Copiando dados para a tabela laravel.chamados: ~1 rows (aproximadamente)
 INSERT INTO `chamados` (`id`, `user_id`, `title`, `text`, `status`, `deleted_at`, `created_at`, `updated_at`) VALUES
-	(7, 1, 'Chamado 2', 'Chamado 2', 'FIN', NULL, '2025-09-04 06:42:25', '2025-09-04 06:43:40'),
-	(8, 1, 'Chamado 3', 'Chamado 3', 'DEV', NULL, '2025-09-04 06:43:38', '2025-09-04 06:44:45'),
-	(9, 2, 'Chamado 1', 'chamado 2', 'PEN', NULL, '2025-09-04 06:44:34', '2025-09-04 06:44:34');
+	(1, 1, 'teste', 'teste', 'FIN', NULL, '2025-09-04 07:26:31', '2025-09-04 07:26:33');
+
+-- Copiando estrutura para tabela laravel.migrations
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Copiando dados para a tabela laravel.migrations: ~2 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(3, '2025_09_02_204058_create_users_table', 1),
-	(4, '2025_09_02_204434_create_users_chamados', 1);
+	(1, '2025_09_02_204058_create_users_table', 1),
+	(2, '2025_09_02_204434_create_users_chamados', 1);
+
+-- Copiando estrutura para tabela laravel.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Copiando dados para a tabela laravel.users: ~2 rows (aproximadamente)
 INSERT INTO `users` (`id`, `username`, `password`, `last_login`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'teste1@teste.com', '$2y$12$SsfvIfAuiStQ46b2KKbRE.4uD9xVgyiKTBnkPBF.AL5n3I/R70wlG', '2025-09-04 06:43:33', '2025-09-03 15:10:50', '2025-09-04 06:43:33', NULL),
-	(2, 'teste2@teste.com', '$2y$12$U9j7ILRmXkAgsGwpQRi1DuKvRAMiVVNQQ/s.6pANMljiDC1KFQ5Bm', '2025-09-04 06:43:46', '2025-09-03 15:10:50', '2025-09-04 06:43:46', NULL);
+	(1, 'teste1@teste.com', '$2y$12$GE57m1Tt/Pehh/NqvDD/s.mFaeMj5OYaMtgbC5UNGevrQW3V40/x2', '2025-09-04 07:26:25', '2025-09-04 07:26:21', '2025-09-04 07:26:25', NULL),
+	(2, 'teste2@teste.com', '$2y$12$vqqdRB6bEvXX2dTMFBYzderHH5IE3rBU72w557LE28B1qXejb3dYK', NULL, '2025-09-04 07:26:21', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;

cc7f40034827f670212639e108748032b161444f
gawa <gwcs@cin.ufpe.br>
2025-09-04 00:06:00 -0300

    [ AuthController + MainControler ]

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,17 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\alert;
+use function PHPUnit\Framework\isNull;
 
 class AuthController extends Controller
 {
     public function login()
     {
-        echo 'login';
+        return view('login');
+    }
+
+    public function loginSub(Request $request)
+    {
+        $userModel = new User();
+
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password'  => 'required|min:6|max:12'
+            ],
+            [
+                'text_username.required' => 'Username is Required',
+                'text_username.email' => 'Invalid Email',
+                'text_password.required' => 'Password is Required',
+                'text_password.min' => 'Invalid Password',
+                'text_password.max' => 'Invalid Password',
+            ]
+            );
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+
+        $user = User::where('username', $username)
+                    ->where('deleted_at', NULL)
+                    ->first();
+
+        if(!$user){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Wrong Username/Password combination');
+        }
+        
+        if(!password_verify($password, $user->password)){
+            return redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Wrong Username/Password combination');
+        }
+
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        session([
+            'user' =>   [ 
+                'id'        => $user->id,
+                'username'  => $user->username
+            ]  
+        ]);
+
+        return redirect()->to('/');
     }
 
     public function logout()
     {
-        echo 'logout';
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
--- src/app/Http/Controllers/MainController.php
+++ src/app/Http/Controllers/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chamado;
+use App\Models\User;
 use Dotenv\Parser\Value;
 use Illuminate\Http\Request;
 
@@ -9,5 +11,31 @@ use function PHPUnit\Framework\isNull;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
 
+        $chamados =  [
+            'PEN' => Chamado::where('status', 'PEN')->get(),
+            'DEV' => Chamado::where('status', 'DEV')->get(),
+            'FIN' => Chamado::where('status', 'FIN')->get()
+        ];
+
+        return view('home', [
+            'user' => $user,
+            'chamados' => $chamados,
+        ]);
+    }
+
+    public function createChamado() {}
+
+    public function updateStatus(Request $request, $id)
+    {
+        $chamado = Chamado::findOrFail($id);
+        $chamado->status = $request->status;
+        $chamado->save();
+
+        return response()->json(['success' => true]);
+    }
 }

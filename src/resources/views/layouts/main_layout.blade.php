aed07700e7898d28018bdecc988a0b6ba5f59143
gawa <gwcs@cin.ufpe.br>
2025-09-04 00:09:28 -0300

    [ Layout + Header ]

--- src/resources/views/layouts/main_layout.blade.php
+++ src/resources/views/layouts/main_layout.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CHAMADOS</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href= "{{ asset('assets/fontawesome/css/all.min.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png')}}" type="image/png">
+    <link rel="stylesheet" href= "{{ asset('assets/css/style.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/zebra.png')}}" type="image/png">
 </head>
 <body>
 
     @yield('content')
 
     <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
+    <script src="js/sortable.js"></script>
 </body>
 </html>
+

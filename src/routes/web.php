75e895a417787befd5f0bab1820cceccf87cd8bb
gawa <gwcs@cin.ufpe.br>
2025-09-04 00:10:35 -0300

    [ Routes ]

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-//Auth routes
+Route::middleware([CheckIsNotLogged::class])->group(function(){
+    Route::get('/login',[AuthController::class, 'login']);
+    Route::post('/loginSub', [AuthController::class, 'loginSub']);
+});
 
-Route::get('/login',[AuthController::class, 'login']);
-Route::get('/logout',[AuthController::class, 'logout']);
+Route::middleware([CheckIsLogged::class])->group(function(){
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/createChamado', [MainController::class, 'createChamado'])->name('createChamado');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/chamados/{id}/status', [MainController::class, 'updateStatus'])->name('chamados.updateStatus');
+});

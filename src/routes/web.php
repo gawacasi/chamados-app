15a5b0534a0b00b73571bea72b52f41e5de5a316
gawa <gwcs@cin.ufpe.br>
2025-09-04 03:47:47 -0300

    [ Routes ]

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,14 +6,24 @@ use App\Http\Middleware\CheckIsLogged;
 use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([CheckIsNotLogged::class])->group(function(){
-    Route::get('/login',[AuthController::class, 'login']);
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+    Route::get('/login', [AuthController::class, 'login']);
     Route::post('/loginSub', [AuthController::class, 'loginSub']);
 });
 
-Route::middleware([CheckIsLogged::class])->group(function(){
+Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
-    Route::get('/createChamado', [MainController::class, 'createChamado'])->name('createChamado');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    //CRUD
+    Route::get('/create-chamado', [MainController::class, 'createChamado'])->name('createChamado');
+    Route::post('/createChamadoSubmit', [MainController::class, 'createChamadoSubmit'])->name('createChamadoSubmit');
+    Route::post('/editSubmit', [MainController::class, 'editSubmit'])->name('editSubmit');
+    Route::get('/edit/{id}', [MainController::class, 'edit'])->name('edit');
+    
+    Route::get('/delete/{id}', [MainController::class, 'delete'])->name('delete');
+    Route::get('/delete-confirm/{id}', [MainController::class, 'deleteConfirm'])->name('deleteConfirm');
+
+    //kanban
     Route::post('/chamados/{id}/status', [MainController::class, 'updateStatus'])->name('chamados.updateStatus');
 });
